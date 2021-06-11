bbe73ab63387bceb16dc7601048fd1c664ff2586
DCI_Developement <worksintriton@gmail.com>
2021-06-11 14:55:25 +0530

    Major Ui Update admin

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastrManager } from 'ng6-toastr-notifications';
-
 declare var $:any;
+
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
@@ -24,14 +24,60 @@ export class AdminComponent implements OnInit {
   iframe: any;
   prp: any;
   trc: any;
+  scrollTop:any;
 
   constructor(private toastr:ToastrManager, private router:Router) { }
 
   ngOnInit(): void {
-    $("#sidebarToggle").on("click", function(e) {
-      e.preventDefault();
-      $("div").toggleClass("sb-sidenav-toggled");
-    });
+            // Toggle the side navigation
+      $("#sidebarToggle, #sidebarToggleTop").on('click', function(e:any) {
+        $("body").toggleClass("sidebar-toggled");
+        $(".sidebar").toggleClass("toggled");
+        if ($(".sidebar").hasClass("toggled")) {
+          $('.sidebar .collapse').collapse('hide');
+        };
+      });
+
+      // Close any open menu accordions when window is resized below 768px
+      $(window).resize(function() {
+        if ($(window).width() < 768) {
+          $('.sidebar .collapse').collapse('hide');
+        };
+        
+        // Toggle the side navigation when window is resized below 480px
+        if ($(window).width() < 480 && !$(".sidebar").hasClass("toggled")) {
+          $("body").addClass("sidebar-toggled");
+          $(".sidebar").addClass("toggled");
+          $('.sidebar .collapse').collapse('hide');
+        };
+      });
+
+      // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
+      $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', (e:any) => {
+        if ($(window).width() > 768) {
+          var e0 = e.originalEvent,
+            delta = e0.wheelDelta || -e0.detail;
+          this.scrollTop += (delta < 0 ? 1 : -1) * 30;
+          e.preventDefault();
+        }
+      });
+      $(document).on('scroll', () => {
+        var scrollDistance = $(this).scrollTop();
+        if (scrollDistance > 100) {
+          $('.scroll-to-top').fadeIn();
+        } else {
+          $('.scroll-to-top').fadeOut();
+        }
+      });
+
+      // Smooth scrolling using jQuery easing
+      $(document).on('click', 'a.scroll-to-top', (e:any) => {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+          scrollTop: ($($anchor.attr('href')).offset().top)
+        }, 1000, 'easeInOutExpo');
+        e.preventDefault();
+      });
   }
 
   logout(){
